30ef26888260c4350834cb5d0e7fb42fe2d3700d
Katie <adlerk8@gmail.com>
2020-11-04 15:17:17 -0700

    in progress

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const massive = require('massive');
 const app = express();
 const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
+const {getAll, getOne, update, create, delete} = require('./product_controller') 
+
 massive({
     connectionString: CONNECTION_STRING,
     ssl: {rejectUnauthorized: false}
@@ -15,5 +17,15 @@ massive({
 });
 
 app.use(express.json());
+
+app.get('/api/products', getAll);
+
+app.get('/api/products/:id', getOne);
+
+app.put('/api/products/:id', update);
+
+app.post('/api/products', create);
+
+app.delete('/api/products/:id', delete);
 
 app.listen(SERVER_PORT, () => {console.log(`Server is listening: port ${SERVER_PORT}`)});
