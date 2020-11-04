4cae29f6bc9e54fcb9cd5404cc3feb4aaf77b695
Katie <adlerk8@gmail.com>
2020-11-04 14:15:55 -0700

    in progress

--- index.js
+++ index.js
@@ -4,21 +4,16 @@ const massive = require('massive');
 const app = express();
 const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
-app.use(express.json());
-
 massive({
     connectionString: CONNECTION_STRING,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    ssl: {rejectUnauthorized: false}
 }).then(db => {
     app.set('db', db)
     console.log('Successfully connected to server')
 }).catch((err) => {
     console.log(err)
-    res.status(500).send()
 });
 
-
+app.use(express.json());
 
 app.listen(SERVER_PORT, () => {console.log(`Server is listening: port ${SERVER_PORT}`)});
