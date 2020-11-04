ced4c77c49669b97f9c005c2e6ac493a8a55e49a
Katie <adlerk8@gmail.com>
2020-11-04 14:14:41 -0700

    in progress

--- index.js
+++ index.js
@@ -6,4 +6,19 @@ const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
 app.use(express.json());
 
-app.listen(SERVER_PORT, () => {console.log(`Server is listening: port ${SERVER_PORT}`)};)
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}).then(db => {
+    app.set('db', db)
+    console.log('Successfully connected to server')
+}).catch((err) => {
+    console.log(err)
+    res.status(500).send()
+});
+
+
+
+app.listen(SERVER_PORT, () => {console.log(`Server is listening: port ${SERVER_PORT}`)});
