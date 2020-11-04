81a57c7dd88408f21aeb162b8f8915485b71f506
Katie <adlerk8@gmail.com>
2020-11-04 15:23:41 -0700

    in progress

--- product_controller.js
+++ product_controller.js
@@ -1,7 +1,9 @@
 module.exports = {
     create: (req, res) => {
         const db = req.app.get('db');
-        db.create_product()
+        const {name, description, price, image_url} = req.body;
+        
+        db.create_product([name, description, price, image_url])
         .then(() => res.status(200))
         .catch(err => {console.log(err)
         res.status(500).send("Oh snap! Something's not right...")
@@ -9,7 +11,9 @@ module.exports = {
     },
     getOne: (req, res) => {
         const db = req.app.get('db');
-        db.read_product()
+        const {id} = req.params;
+
+        db.read_product(id)
         .then(product => res.status(200).send(product))
         .catch(err => {console.log(err)
         res.status(500).send("Oh snap! Something's not right...")
@@ -17,6 +21,7 @@ module.exports = {
     },
     getAll: (req, res) => {
         const db = req.app.get('db');
+        
         db.read_products()
         .then(products => res.status(200).send(products))
         .catch(err => {console.log(err)
@@ -25,7 +30,9 @@ module.exports = {
     },
     update: (req, res) => {
         const db = req.app.get('db');
-        db.update_product()
+        const {id} = req.params;
+
+        db.update_product(id)
         .then(() => res.status(200))
         .catch(err => {console.log(err)
         res.status(500).send("Oh snap! Something's not right...")
@@ -33,7 +40,9 @@ module.exports = {
     },
     delete: (req, res) => {
         const db = req.app.get('db');
-        db.delete_product()
+        const {id} = req.params;
+        
+        db.delete_product(id)
         .then(() => res.status(200))
         .catch(err => {console.log(err)
         res.status(500).send("Oh snap! Something's not right...")
