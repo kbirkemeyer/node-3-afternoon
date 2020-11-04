4c633e7a1e03a2b423953b6536e8708fb8e00e8c
Katie <adlerk8@gmail.com>
2020-11-04 15:07:45 -0700

    in progress

---
+++ product_controller.js
@@ -0,0 +1,42 @@
+module.exports = {
+    create: (req, res) => {
+        const db = req.app.get('db');
+        db.create_product()
+        .then(() => res.status(200))
+        .catch(err => {console.log(err)
+        res.status(500).send("Oh snap! Something's not right...")
+        })
+    },
+    getOne: (req, res) => {
+        const db = req.app.get('db');
+        db.read_product()
+        .then(() => res.status(200))
+        .catch(err => {console.log(err)
+        res.status(500).send("Oh snap! Something's not right...")
+        })
+    },
+    getAll: (req, res) => {
+        const db = req.app.get('db');
+        db.read_products()
+        .then(() => res.status(200))
+        .catch(err => {console.log(err)
+        res.status(500).send("Oh snap! Something's not right...")
+        })
+    },
+    update: (req, res) => {
+        const db = req.app.get('db');
+        db.update_product()
+        .then(() => res.status(200))
+        .catch(err => {console.log(err)
+        res.status(500).send("Oh snap! Something's not right...")
+        })
+    },
+    delete: (req, res) => {
+        const db = req.app.get('db');
+        db.delete_product()
+        .then(() => res.status(200))
+        .catch(err => {console.log(err)
+        res.status(500).send("Oh snap! Something's not right...")
+        })
+    }
+}
